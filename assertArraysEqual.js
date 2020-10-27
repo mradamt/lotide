d8d683ce305e36a953c853878d48972346524ba5
adam <mr.adamtyler@gmail.com>
2020-10-27 04:03:58 +0000

    Write assertArraysEqual function to compare two arrays and output Assertion Passed/Failed messages with emojis

---
+++ assertArraysEqual.js
@@ -0,0 +1,30 @@
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  let len = arr1.length;
+  if (len === arr2.length) {
+    for (let i = 0; i < len; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+// TEST CASES
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // pass
+assertArraysEqual([1], [1]); // pass
+assertArraysEqual([], []); // pass
+assertArraysEqual([1, 2, "3"], [1, 2, "3"]); // pass
+assertArraysEqual([1, 2, 3], [1, 2, 4]); // fail

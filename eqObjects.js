a537e822c49c45069180f837fe0f6740bf4f06f5
adam <mr.adamtyler@gmail.com>
2020-10-29 02:54:38 +0000

    Lint and cleanup

--- eqObjects.js
+++ eqObjects.js
@@ -11,18 +11,18 @@ const eqArrays = (arr1, arr2) => {
   let len = arr1.length;
   if (len !== arr2.length) {
     return false;
-  } 
+  }
   for (let i = 0; i < len; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
-  return true; 
+  return true;
 };
 
 
 // eqObjects takes in two objects and returns true or false, based on a perfect match
-const eqObjects = function (object1, object2) {
+const eqObjects = function(object1, object2) {
   const keys = Object.keys(object1);
   if (keys.length !== Object.keys(object2).length) {
     return false;
@@ -31,9 +31,7 @@ const eqObjects = function (object1, object2) {
     // console.log("object1[key]:", object1[key], "object2[key]:", object2[key])
     if (Array.isArray(object1[key])) {
       // console.log("isArray=TRUE:", object1[key])
-      if (eqArrays(object1[key], object2[key])) {
-      //   return false;
-      // } else {
+      if (eqArrays(object1[key], object2[key])) { // If both arrays are equal (TRUE), break to next key
         break;
       }
     }
@@ -42,26 +40,26 @@ const eqObjects = function (object1, object2) {
     }
   }
   return true;
-}
+};
 
 
 // TEST CASES
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true)
+assertEqual(eqObjects(ab, ba), true);
 // console.log(eqObjects(ab, ba)); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false)
+assertEqual(eqObjects(ab, abc), false);
 // console.log(eqObjects(ab, abc)); // => false
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true)
+assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 const dc2 = { c: "1", d: 1 };
-assertEqual(eqObjects(cd, cd2), false)
-assertEqual(eqObjects(cd2, dc2), false)
+assertEqual(eqObjects(cd, cd2), false);
+assertEqual(eqObjects(cd2, dc2), false);
 // console.log(eqObjects(cd, dc))
 // console.log(eqObjects(cd2, dc2))

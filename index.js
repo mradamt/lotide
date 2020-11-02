b2327a2a6fedd008619aa70691cf00bc70f31014
adam <mr.adamtyler@gmail.com>
2020-11-02 23:26:24 +0000

    Create index.js file with module.exports for all utility files

---
+++ index.js
@@ -0,0 +1,35 @@
+const assertObjectsEqual = require ('./assertObjectsEqual');
+const countLetters = require ('./countLetters');
+const countOnly = require ('./countOnly');
+const eqObjects = require ('./eqObjects');
+const findKey = require ('./findKey');
+const findKeyByValue = require ('./findKeyByValue');
+const flatten = require ('./flatten');
+const head = require('./head');
+const letterPositions = require ('./letterPositions');
+const map = require ('./map');
+const middle = require('./middle');
+const tail = require('./tail');
+const takeUntil = require ('./takeUntil');
+const without = require ('./without');
+
+
+module.exports = {
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  head: head,
+  index: index,
+  letterPositions: letterPositions,
+  map: map,
+  middle: middle,
+  tail: tail,
+  takeUntil: takeUntil,
+  without: without,
+};
+
+

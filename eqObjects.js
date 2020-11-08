d55cc273863cabb6f022e79baee6de248ad499a3
adam <mr.adamtyler@gmail.com>
2020-11-08 20:50:41 +0000

    Refactor: rearrange and neaten up code

--- eqObjects.js
+++ eqObjects.js
@@ -2,17 +2,16 @@ const eqArrays = require('./eqArrays');
 
 // eqObjects takes in two objects and returns true or false, based on a perfect match
 const eqObjects = function(object1, object2) {
+  // Check if both objects have the same number of properties
   const keys = Object.keys(object1);
   if (keys.length !== Object.keys(object2).length) {
     return false;
   }
+  // Compare properties of each object
   for (const key of keys) {
-    // console.log("object1[key]:", object1[key], "object2[key]:", object2[key])
-    if (Array.isArray(object1[key])) {
-      // console.log("isArray=TRUE:", object1[key])
-      if (eqArrays(object1[key], object2[key])) { // If both arrays are equal (TRUE), break to next key
-        break;
-      }
+    // If both arrays are equal (TRUE), break to next key (don't do strict equal check)
+    if (Array.isArray(object1[key]) && eqArrays(object1[key], object2[key])) {
+      break;
     }
     if (object1[key] !== object2[key]) {
       return false;

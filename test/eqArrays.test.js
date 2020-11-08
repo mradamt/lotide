9e918a20b76d6f033ab8e0ffdddaa78cd8c20a0b
adam <mr.adamtyler@gmail.com>
2020-11-08 21:41:33 +0000

    Reword test scenarios to be more descriptive

--- test/eqArrays.test.js
+++ test/eqArrays.test.js
@@ -2,23 +2,23 @@ const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
 describe("#eqArrays", () => {
-  it("returns true for eqArrays([1, 2, 3], [1, 2, 3]))", () => {
+  it("returns true for equal arrays of numbers)", () => {
     assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
   });
   
-  it("returns false for eqArrays([1, 2, 3], [3, 2, 1]))", () => {
+  it("returns false for arrays with same data but different order))", () => {
     assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
   });
   
-  it('returns true for eqArrays(["1", "2", "3"], ["1", "2", "3"]))', () => {
+  it('returns true for equal arrays of strings)', () => {
     assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
   });
   
-  it('returns true for eqArrays(["1", "2", "3"], ["1", "2", 3]))', () => {
+  it('returns false for arrays of equal implicit values but different data types)', () => {
     assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
   });
   
-  it('returns true for eqArrays([1, 2, 3, "ffo"], [1, 2, 3, "ffo"]))', () => {
+  it('returns true for equal arrays with mixed data types)', () => {
     assert.strictEqual(eqArrays([1, 2, 3, "ffo"], [1, 2, 3, "ffo"]), true);
   });
 });

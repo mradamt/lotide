a76bc0d4edb11e241708441cf55318fbe20ba739
adam <mr.adamtyler@gmail.com>
2020-11-08 21:33:00 +0000

    Refactor to handle nested objects; additional testing to cover this

--- eqObjects.js
+++ eqObjects.js
@@ -9,14 +9,27 @@ const eqObjects = function(object1, object2) {
   }
   // Compare properties of each object
   for (const key of keys) {
-    // If both arrays are equal (TRUE), break to next key (don't do strict equal check)
+    // If both values are arrays and are equal, break to next key (skip further checks)
     if (Array.isArray(object1[key]) && eqArrays(object1[key], object2[key])) {
       break;
     }
+    
+    // If both values are objects and not arrays, call eqObjects on these objects
+    if (typeof object1[key] === 'object' && !Array.isArray(object1[key]) &&
+        typeof object2[key] === 'object' && !Array.isArray(object2[key])) {
+      
+      if (eqObjects(object1[key], object2[key])) {
+        break;
+      }
+    }
+
+    // This pair of values can be strictly compared to each other
     if (object1[key] !== object2[key]) {
       return false;
     }
   }
+
+  // All key-value pairs of all nested objects (even if values are arrays) are equal
   return true;
 };
 
--- test/eqObjects.test.js
+++ test/eqObjects.test.js
@@ -2,23 +2,39 @@ const assert = require('chai').assert;
 const eqObjects = require('../eqObjects');
 
 describe("#eqObjects", () => {
-  it('returns true for ({ a: "1", b: "2" }, { b: "2", a: "1" })', () => {
+  it('returns true for equal objects with keys out of order', () => {
     assert.equal(eqObjects({ a: "1", b: "2" }, { b: "2", a: "1" }), true);
   }),
   
-  it('returns false for ({ a: "1", b: "2" }, { a: "1", b: "2", c: "3" })', () => {
+  it('returns false for objects with different number of keys', () => {
     assert.strictEqual(eqObjects({ a: "1", b: "2" }, { a: "1", b: "2", c: "3" }), false);
   }),
 
-  it('returns true for ({ c: "1", d: ["2", 3] }, { d: ["2", 3], c: "1" })', () => {
+  it('returns true for equal objects containing equal arrays', () => {
     assert.strictEqual(eqObjects({ c: "1", d: ["2", 3] }, { d: ["2", 3], c: "1" }), true);
   }),
 
-  it('returns false for ({ c: "1", d: ["2", 3] }, { c: "1", d: ["2", 3, 4] })', () => {
+  it('returns false for equal objects containing unequal arrays', () => {
     assert.strictEqual(eqObjects({ c: "1", d: ["2", 3] }, { c: "1", d: ["2", 3, 4] }), false);
   }),
 
-  it('returns false for ({ c: "1", d: ["2", 3, 4] }, { c: "1", d: 1 })', () => {
+  it('returns false for objects one with array one without', () => {
     assert.strictEqual(eqObjects({ c: "1", d: ["2", 3, 4] }, { c: "1", d: 1 }), false);
   });
+
+  it('returns true for equal objects that have one layer of equal objects as values', () => {
+    assert.equal(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+  }),
+
+  it('returns true for equal objects that have several layers of equal objects as values', () => {
+    assert.equal(eqObjects({ a: {y: { z: 1 }, v: {w: {x: 33}}}, b: 2 }, { a: {v: {w: {x: 33}}, y: { z: 1 }}, b: 2 }), true);
+  }),
+
+  it('returns false for equal objects that have one layer of unequal objects as values', () => {
+    assert.equal(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+  }),
+
+  it('returns false for equal objects, one with an object as a value, the other without', () => {
+    assert.equal(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false);
+  })
 });
